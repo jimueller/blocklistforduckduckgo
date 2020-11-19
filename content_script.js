ba68b0eb3d48d8b10c7e5473e280411d22753d90
Jim Mueller <jim@jimueller.com>
2020-11-19 14:30:47 -0600

    Adds styling and basic removal functionality

--- content_script.js
+++ content_script.js
@@ -1,12 +1,32 @@
 // Put all the javascript code here, that you want to execute after page load.
-
+let blockedDomains = [];
 
 browser.storage.onChanged.addListener((changes, area) => {
-    if(area === 'local' && 'blockedDomains' in changes){
-        removeResults(changes.blockedDomains.newValue);
-    }
+  if (area === "local" && "blockedDomains" in changes) {
+    removeResults(changes.blockedDomains.newValue);
+  }
 });
 
-function removeResults(blockedDomains){
-    console.log("removing", blockedDomains);
+function removeResults(domains) {
+  const results = document.querySelectorAll(".result");
+  results.forEach((r) => {
+    r.style.display = "";
+    if (domains.includes(r.dataset.domain)) {
+      r.style.display = "none";
+    }
+  });
 }
+
+async function loadDomains() {
+  const results = await browser.storage.local.get("blockedDomains");
+  if (results && results.blockedDomains) {
+    blockedDomains = results.blockedDomains;
+  }
+}
+
+async function init() {
+  await loadDomains();
+  removeResults(blockedDomains);
+}
+
+window.addEventListener("load", () => init().catch((e) => console.error(e)));
--- popup.js
+++ popup.js
@@ -1,73 +1,101 @@
-const domainInput = document.querySelector('#domain');
-const saveButton = document.querySelector('#save');
-const domainList = document.querySelector('#blocked-domains');
+const domainInput = document.querySelector("#domain");
+const saveButton = document.querySelector("#save");
+const domainList = document.querySelector("#blocked-domains");
 
 async function storeSettings(domainsValue) {
-    const blockedDomains = domainsValue.split("\n") || [];
+  const blockedDomains = domainsValue.split("\n") || [];
 
-    console.log(blockedDomains);
-    await browser.storage.local.set({
-        blockedDomains
-    });
+  console.log(blockedDomains);
+  await browser.storage.local.set({
+    blockedDomains,
+  });
 }
 
 async function loadDomains() {
-    let domains = [];
-    const results =  await browser.storage.local.get('blockedDomains');
-    if(results && results.blockedDomains){
-        domains = JSON.parse(results.blockedDomains);
-    }
-    console.log("returning domains:", domains);
-    return domains;
+  let domains = [];
+  const results = await browser.storage.local.get("blockedDomains");
+  if (results && results.blockedDomains) {
+    domains = JSON.parse(results.blockedDomains);
+  }
+  console.log("returning domains:", domains);
+  return domains;
 }
 
 async function storeDomains(domains) {
-    const blockedDomains = JSON.stringify(domains);
-    await browser.storage.local.set({
-        blockedDomains
-    });
+  const blockedDomains = JSON.stringify(domains);
+  await browser.storage.local.set({
+    blockedDomains,
+  });
 }
 
 async function saveDomain(domain) {
-    const domains = await loadDomains();
-    domains.push(domain);
-    await storeDomains(domains);
+  const domains = await loadDomains();
+  domains.push(domain);
+  await storeDomains(domains);
 }
 
-async function onSaveClick(){
-    let domainText = domainInput.value;
-    await saveDomain(domainText);
-    const domains = await loadDomains();
-    renderDomainList(domains);
-    domainInput.value = "";
+async function deleteDomain(domain) {
+  const domains = await loadDomains();
+  const updated = domains.filter((d) => {
+    return d !== domain;
+  });
+  await storeDomains(updated);
+  const savedDomains = await loadDomains();
+  renderDomainList(savedDomains);
 }
 
-function renderDomainList(domains){
-    console.log("Rendering domains", domains);
-    console.log("domainsList", domainList);
-    domainList.innerHTML = "";
-    console.log('after blanking domains list');
-    
-    domains.map(d => {
-        console.log('inserting domain');
-        const li = document.createElement("li");
-        li.appendChild(document.createTextNode(d));
-        li.setAttribute("class", "list-domain");
-        const deleteSpan = document.createElement("span");
-        deleteSpan.setAttribute("class", "delete-domain");
-        deleteSpan.setAttribute("data-domain",d);
-        deleteSpan.appendChild(document.createTextNode("\u00d7"));
-        li.appendChild(deleteSpan);
-        domainList.appendChild(li);
-    });
+async function onSaveClick() {
+  let domainText = domainInput.value;
+  await saveDomain(domainText);
+  const domains = await loadDomains();
+  renderDomainList(domains);
+  domainInput.value = "";
 }
 
-saveButton.addEventListener('click', onSaveClick);
+async function onDeleteClick(e) {
+  let domain = e.target.dataset.domain;
+  await deleteDomain(domain);
+}
+
+function onListClick(e) {
+  if (e.target && e.target.matches("span.delete-domain")) {
+    onDeleteClick(e);
+  }
+}
+
+function renderDomainList(domains) {
+  console.log("Rendering domains", domains);
+  console.log("domainsList", domainList);
+  domainList.innerHTML = "";
+  console.log("after blanking domains list");
+
+  domains.map((d) => {
+    console.log("inserting domain");
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(d));
+    li.setAttribute("class", "list-domain");
+    const deleteSpan = document.createElement("span");
+    deleteSpan.setAttribute("class", "delete-domain");
+    deleteSpan.setAttribute("data-domain", d);
+    deleteSpan.appendChild(document.createTextNode("\u00d7"));
+    li.appendChild(deleteSpan);
+    domainList.appendChild(li);
+  });
+}
+
+saveButton.addEventListener("click", onSaveClick);
+domainInput.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13 /*Enter key*/) {
+    e.preventDefault();
+    saveButton.click();
+  }
+});
+domainList.addEventListener("click", onListClick);
 
 async function init() {
-    const domains = await loadDomains();
-    console.log('init domains', domains);
-    renderDomainList(domains);
+  const domains = await loadDomains();
+  console.log("init domains", domains);
+  renderDomainList(domains);
 }
 
-init().catch(e => console.error(e));
+init().catch((e) => console.error(e));
